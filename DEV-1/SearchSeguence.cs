805400b8bb2d597f07d4a541062b02ac808c1c22
OlgaYurkova <olya_yurkovaa@mail.ru>
2018-03-03 16:17:15 +0300

    try 2

--- DEV-1/SearchSeguence.cs
+++ DEV-1/SearchSeguence.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DEV_1
 {
@@ -16,30 +13,31 @@ namespace DEV_1
         static void Main(string[] args)
         {
 
-            if (args.Length>=1 && args.Length < 3)
+            if (args.Length==1)
             {
                 string inputString = args[0];
                 int counterOfChars = 1;
                 int maxSequence = 0;
-                for (int i = 0; i < inputString.Length-1 ; i++)
-                {
-                    if (inputString[i] == inputString[i + 1])
+
+                  for (int i = 0; i < inputString.Length- 1 ; i++)
                     {
+                     if (inputString[i] == inputString[i + 1])
+                      {
                         counterOfChars++;
-                    }
-                    else
-                    {
-                        if (counterOfChars > maxSequence)
-                        {
-                            maxSequence = counterOfChars;
-                        }
+                      }
+                      else
+                     {
+                             if (counterOfChars > maxSequence)
+                             {
+                                 maxSequence = counterOfChars;
+                             }
                         counterOfChars = 1;
-                    }
-                }
-                if (counterOfChars > maxSequence)
-                {
-                    maxSequence = counterOfChars;
-                }
+                        }
+                     }
+                      if (counterOfChars > maxSequence)
+                     {
+                        maxSequence = counterOfChars;
+                     }
                 Console.WriteLine("Maximum sequence of chars in string = {0}", maxSequence);
             }
             else
