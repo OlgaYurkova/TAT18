f5daabe9db2f59552144d4ae98473b13f1e07ad5
OlgaYurkova <olya_yurkovaa@mail.ru>
2018-03-04 16:46:07 +0300

    try 2

--- DEV-2/SearchForEvenIndices.cs
+++ DEV-2/SearchForEvenIndices.cs
@@ -1,32 +1,29 @@
 ﻿using System;
 using System.Text;
-
-
 namespace DEV_2
 {
     /// <summary>
-    ///The SequenceSearch class is the main class of the program that works with the string
+    ///The SearchForEvenIndices class is the main class of the program that works with the string
     ///Outputs to the console the original string consisting of characters with even indices
     /// </summary>
     class SearchForEvenIndices
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
             Console.WriteLine("input string :");
             StringBuilder inputString = new StringBuilder(Console.ReadLine());
+
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                inputString.Remove(i, 1);
+            }
 
             if (inputString.Length == 0)
             {
                 Console.WriteLine("The string wasn't entered");
             }
 
-            for (int i = 1; i < inputString.Length; i++)
-            {
-                inputString.Remove(i, 1);
-            }
             Console.WriteLine(inputString);
-          
         }
     }
 }
