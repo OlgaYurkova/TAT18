a19c5b903d8c545a7db135bf8176a9437769a74e
OlgaYurkova <olya_yurkovaa@mail.ru>
2018-03-07 17:19:38 +0300

    try 1

---
+++ DEV-3/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace DEV_3
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BigInteger inputNumber = BigInteger.Parse(args[0]);
+                int primаryOfNewSystem = Int32.Parse(args[1]);
+                ConverterToOtherSystem converter = new ConverterToOtherSystem(primаryOfNewSystem);
+
+                Console.WriteLine(converter.GetChangedNumber(inputNumber));
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
