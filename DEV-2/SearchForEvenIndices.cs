a9cae8185bce4dd0b1476f62a96447de2f62895e
OlgaYurkova <olya_yurkovaa@mail.ru>
2018-03-16 17:53:07 +0300

    test Xunit

---
+++ DEV-2/SearchForEvenIndices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    /// <summary>
+    /// The SearchForEvenIndices class is the main class of the program that works with the string
+    /// Outputs to the console the original string consisting of characters with even indices
+    /// </summary>
+    public class SearchForEvenIndices
+    {
+        static void Main()
+        {
+            Console.WriteLine("input string :");
+            String input = Console.ReadLine();
+            SearchForEvenIndices index = new SearchForEvenIndices();
+            Console.WriteLine(index.FormatString(input));
+        }
+
+        public string FormatString(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            StringBuilder inputString = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                inputString.Append(input[i]);
+            }
+            return inputString.ToString();
+        }
+    }
+}
---
+++ DEV-2/xUnitDEV-2.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using System;
+using DEV_2;
+
+namespace xUnitTest
+{
+    public class DEV_2
+    {
+        private SearchForEvenIndices objectBuilder = new SearchForEvenIndices();
+
+        [Fact]
+        public void SearchForEvenIndicesTest()
+        {
+            string inputString = "KoTiki $12 _^%# ' ";
+            string result = "KTk 1 ^#'";
+            SearchForEvenIndices input = new SearchForEvenIndices();
+            Assert.Equal(input.FormatString(inputString), result);
+        }
+
+        [Fact]
+        public void StringArgumentNullTest()
+        {
+            string inputString = null;
+            Assert.Throws<ArgumentNullException>(() => objectBuilder.FormatString(inputString));
+        }
+    }
+}
