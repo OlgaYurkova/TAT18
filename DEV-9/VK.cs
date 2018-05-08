16c63752c66fd77c2acd6151e31197e69a14cac3
OlgaYurkova <olya_yurkovaa@mail.ru>
2018-05-08 09:43:40 +0300

    VK login

---
+++ DEV-9/VK.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace DEV_9
+{
+    class VK
+    {
+        static void Main()
+        {
+          Console.WriteLine("Enter login: ");
+          string login = Console.ReadLine();
+          Console.WriteLine("Enter password: ");
+          string password = Console.ReadLine();
+
+          LoginPage loginPage = new LoginPage();
+          loginPage.loginInformation(login, password);
+        }
+    }
+}
