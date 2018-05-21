ee4ec98bcba7a1f628f714d625cbd2af019bc968
OlgaYurkova <olya_yurkovaa@mail.ru>
2018-05-21 21:49:31 +0300

    Work

---
+++ work/RectangleArea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task2
+{
+    public class RectangleArea
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Length of the rectangle");
+                int length = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Width of the rectangle");
+                int width = int.Parse(Console.ReadLine());
+
+                int area = length * width;
+
+                if( area <= 0 )
+                {
+                    throw new Exception("Invalid input");
+                }
+               
+                Console.WriteLine("{0} * {1} = {2}", length, width, area);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+        }
+    }
+}
